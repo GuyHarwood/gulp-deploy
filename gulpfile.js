4d4f949844b85e15f250306589c861fc32ab3ed0
Guy Harwood <gpharwood@gmail.com>
2016-06-22 20:22:29 +0100

    basic gulp structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,22 @@
-ECHO is on.
+"use strict";
+
+let gulp = require('gulp'),
+    msbuild = require('gulp-msbuild'),
+    nunit = require('gulp-nunit-runner'),
+    runSequence = require('run-sequence'),
+    configTransform = require('gulp-config-transform'),
+    sc = require('windows-service-controller'),
+    _ = require('underscore'),
+    deployConfig = require('./deploy/deploy.json'),
+    components = require('./deploy/components.json'),
+    q = require('q'),
+    iis = require('iis'),
+    yargs = require('yargs')
+    .usage('Usage: $0 -buildVersion [major.minor.patch]')
+    //.demand(['buildVersion'])
+    .argv;
+
+    gulp.task('default', function(done){
+        console.log('hello from gulp');
+        done();
+    })
