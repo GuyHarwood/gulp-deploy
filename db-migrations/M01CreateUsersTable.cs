4de13d33c5251129ec2174f37dc5f08a13ce4e5b
Guy Harwood <gpharwood@gmail.com>
2016-06-22 20:29:23 +0100

    add migration project with example create table migration

---
+++ db-migrations/M01CreateUsersTable.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace db_migrations
+{
+    [Migration(1,"Create Users Table")]
+    public class M01CreateUsersTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Users").WithDescription("We keep the users in this table")
+                .WithColumn("Id").AsInt64().PrimaryKey()
+                .WithColumn("Username").AsString(50).NotNullable()
+                .WithColumn("DateCreated").AsDateTime()
+                .WithDefault(SystemMethods.CurrentDateTime)
+                .NotNullable();
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
